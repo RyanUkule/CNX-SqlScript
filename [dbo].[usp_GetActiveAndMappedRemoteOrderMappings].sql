f0caa9b11f40d3455dbed1ebffee84df2db51241
ryan.han <han.wei@itr.cn>
2017-06-14 18:29:08 +0800

    Initial  [usp_GetActiveAndMappedRemoteOrderMappings].sql

---
+++ [dbo].[usp_GetActiveAndMappedRemoteOrderMappings].sql
@@ -0,0 +1,35 @@
+USE [cnx]
+GO
+/****** Object:  StoredProcedure [dbo].[usp_GetActiveAndMappedRemoteOrderMappings]    Script Date: 2017/06/14 18:24:29 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER PROCEDURE [dbo].[usp_GetActiveAndMappedRemoteOrderMappings]
+AS
+BEGIN
+	SELECT r.OrderId,
+		   r.[RemoteOrderMappingsId],
+		   r.[ExecutePrice],
+		   r.[Quantity],
+		   r.[RemoteExchangeId],
+		   r.[RemoteOrderId],
+		   r.[RemoteExecutePrice],
+		   r.[RemoteExecuteQuantity],
+		   r.[OrderTypeId],
+		   r.[OfferAssetTypeId],
+		   r.[WantAssetTypeId],
+		   r.[RemoteOrderMappingStatusId],
+		   r.[Message],
+		   r.[Retry],
+		   r.[InsertDate],
+		   r.[UpdateDate]
+	FROM RemoteOrderMappings r WITH(NOLOCK)
+	INNER JOIN OrderBook o WITH(NOLOCK) ON r.OrderId = o.OrderId
+	WHERE r.RemoteOrderMappingStatusId = 8 
+		AND r.RemoteExchangeId IS NOT NULL
+		AND r.RemoteOrderId IS NOT NULL
+		AND r.Quantity > 0
+		AND r.ExecutePrice > 0
+		AND o.OrderStatusId = 2
+END
