73f846802fa08c5d26101366e5cb7ec14961f370
Ryan.Han <han.wei@itr.cn>
2017-09-01 16:01:08 +0800

    Completed Task-15

--- Task-15_Deploy HuobiBCC Api/usp_GetAssetTypeIdByExchangeId.sql
+++ Task-15_Deploy HuobiBCC Api/usp_GetAssetTypeIdByExchangeId.sql
@@ -5,18 +5,21 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+/**************************
+Modified by: Ryan.Han
+Modified Date: 2017-08-31
+***************************/
 ALTER PROCEDURE [dbo].[usp_GetAssetTypeIdByExchangeId]
 (
 	@exchangeId int
 )
 AS
 BEGIN
-	SELECT AssetTypeId
-	FROM
-		(SELECT 
-			   CASE OrderTypeId 
-					WHEN 1 THEN WantAssetTypeId
-					WHEN 2 THEN OfferAssetTypeId END AS AssetTypeId 
-		FROM RemoteOrderMappings WITH (NOLOCK) WHERE RemoteExchangeId = @exchangeId) OrderMappings
-	GROUP BY AssetTypeId
+	SELECT 
+		OrderTypeId,
+		WantAssetTypeId,
+		OfferAssetTypeId
+	FROM RemoteOrderMappings WITH (NOLOCK) 
+	WHERE RemoteExchangeId = @exchangeId AND RemoteOrderMappingStatusId = 8
+	group by OrderTypeId,WantAssetTypeId,OfferAssetTypeId
 END
