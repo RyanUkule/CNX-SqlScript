4a64fe6840317e1dd73671eaf2518c5c0666ef39
Ryan.Han <han.wei@itr.cn>
2017-10-23 15:56:33 +0800

    Init Task-19 Script : usp_UpdateCustomerTicketStatuId.sql

---
+++ Task-19_Validate paramter of Api/usp_UpdateCustomerTicketStatuId.sql
@@ -0,0 +1,13 @@
+USE [cnx]
+GO
+/****** Object:  StoredProcedure [dbo].[usp_UpdateCustomerTicketStatuId]    Script Date: 2017/10/23 14:23:46 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER PROCEDURE [dbo].[usp_UpdateCustomerTicketStatuId] @CreateBy int, @TicketId int,@TicketStatuId int 
+as 
+begin
+   Update [CustomerTickets] set [TicketStatusId]=@TicketStatuId ,UpdateDate=getDate() where [CreateBy]=@CreateBy and TicketId=@TicketId
+
+end
